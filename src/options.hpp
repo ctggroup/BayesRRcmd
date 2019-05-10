9d36cf470e9477052fdb4a3159e001da7baee67d
Nathan Collins <nathan.collins@kdab.com>
2019-05-10 12:21:23 +0100

    Default to unlimited concurrency
    The default was originally 1 which means without a --thread option the
    analysis would not be run concurrently.

--- src/options.hpp
+++ src/options.hpp
@@ -23,7 +23,7 @@ public:
     unsigned chainLength;
     unsigned burnin;
     unsigned seed;
-    unsigned numThread;
+    unsigned numThread = 0; // Default to tbb::flow::unlimited
     int numThreadSpawned = 0; // Default to 0, let TBB do its thing
     size_t decompressionNodeConcurrency = 0;
     size_t decompressionTokens = 40;
@@ -55,7 +55,7 @@ public:
         chainLength             = 10000;
         burnin                  = 5000;
         seed                    = static_cast<unsigned int>(std::time(0));
-        numThread               = 1;
+        numThread               = 0;
         numThreadSpawned        = 0;
         decompressionNodeConcurrency = 0;
         decompressionTokens     = 40;
