e660639b825bc8b83b8905c97084db3ccc6d2a92
Nathan Collins <nathan.collins@kdab.com>
2019-06-14 13:36:53 +0100

    Test preprocessing CSV files
    Preprocessing is expected to work with PreprocessDataType::Dense, with
    and without compression.
    
    It is not expected to work with the sparse preprocess data types.
    
    DISABLED_PreprocessCsvDense is disbabled because the test data is not
    available yet.

--- test/ppbayestest.cpp
+++ test/ppbayestest.cpp
@@ -51,3 +51,63 @@ INSTANTIATE_TEST_SUITE_P(PreprocessTests,
                                                   PreprocessDataType::SparseEigen,
                                                   PreprocessDataType::SparseRagged}),
                              ::testing::Bool()));
+
+class DISABLED_PreprocessCsvDense : public ::testing::TestWithParam<bool> {};
+
+TEST_P(DISABLED_PreprocessCsvDense, WithAndWithoutCompression) {
+    const std::string testDataDir(TEST_DATA);
+    Options options;
+    options.analysisType = AnalysisType::Preprocess;
+    options.dataFile = testDataDir + "uk10k_chr1_1mb_transpose.csv";
+    options.inputType = InputType::CSV;
+    options.phenotypeFile = testDataDir + "test.csvphen";
+
+    // Set the test specific values
+    options.compress = GetParam();
+
+    // Clean up old files
+    fs::path ppFile(ppFileForType(options.preprocessDataType, options.dataFile));
+    std::error_code ec;
+    if (fs::exists(ppFile))
+        ASSERT_TRUE(fs::remove(ppFile, ec)) << ec.message();
+
+    fs::path ppIndexFile(ppIndexFileForType(options.preprocessDataType, options.dataFile));
+    if (fs::exists(ppIndexFile))
+        ASSERT_TRUE(fs::remove(ppIndexFile, ec)) << ec.message();
+
+    // Preprocess
+    ASSERT_TRUE(AnalysisRunner::run(options));
+
+    // Validate the output
+    ASSERT_TRUE(fs::exists(ppFile));
+    ASSERT_GT(fs::file_size(ppFile, ec), 0) << ec.message();
+
+    ASSERT_TRUE(fs::exists(ppIndexFile));
+    ASSERT_GT(fs::file_size(ppIndexFile, ec), 0) << ec.message();
+}
+
+INSTANTIATE_TEST_SUITE_P(PreprocessTests,
+                         DISABLED_PreprocessCsvDense,
+                         ::testing::Bool());
+
+class PreprocessCsvSparse : public ::testing::TestWithParam<PreprocessDataType> {};
+
+TEST_P(PreprocessCsvSparse, ExpectingFailureForSparseDataTypes) {
+    const std::string testDataDir(TEST_DATA);
+    Options options;
+    options.analysisType = AnalysisType::Preprocess;
+    options.dataFile = testDataDir + "uk10k_chr1_1mb_transpose.csv";
+    options.inputType = InputType::CSV;
+    options.phenotypeFile = testDataDir + "test.csvphen";
+
+    // Set the test specific values
+    options.preprocessDataType = GetParam();
+
+    // Preprocess
+    ASSERT_FALSE(AnalysisRunner::run(options)) << "Sparse preprocess data types should not be supported with CSV data";
+}
+
+INSTANTIATE_TEST_SUITE_P(PreprocessTests,
+                         PreprocessCsvSparse,
+                         ::testing::ValuesIn({PreprocessDataType::SparseEigen,
+                                              PreprocessDataType::SparseRagged}));
